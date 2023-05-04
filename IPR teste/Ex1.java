05b57ae7948a58d0d8f740ea54828d279ec8f72a
reckiezz <rickramoos1@gmail.com>
2023-05-04 09:22:13 -0300

    Envio da primeira versão

---
+++ IPR teste/Ex1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class l4exer13 {
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("\nDigite a coordenada da abscissa do plano: ");
+        int coorX = Integer.parseInt(input.nextLine());
+
+        System.out.print("\nDigite a coordenada da ordenada do plano: ");
+        int coorY = Integer.parseInt(input.nextLine());
+        
+        String quadrante = apresentaQuadrante(coorX, coorY);
+
+        System.out.print("\nO par ordenado ("+coorX+","+coorY+") está no "+quadrante+"\n\n");
+
+    }
+
+    static String apresentaQuadrante(int x, int y){
+        Scanner input = new Scanner(System.in);
+
+          if(x >= 0 && y >= 0){
+            return "primeiro quadrante.";
+
+        } else if (x < 0 && y >= 0){
+            return "segundo quadrante.";
+
+        } else if (x < 0 && y < 0){
+            return "terceiro quadrante.";
+
+        } else {
+            return "quarto quadrante.";
+        }
+    }
+    
+}
+
